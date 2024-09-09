51cac4abefd06110f293b93b3d839bac414c1d1a
arokua <trannguyenphucanh@gmail.com>
2024-09-09 14:12:14 +1000

    second commit
    test hosting

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default {
+  base: '/arokua.github.io/',
+  build: {
+    outDir: 'dist'
+  },
+  plugins:[react()]
+}
